f23459009768549deb36353c2451587d007aca8b
hpfull-yamucha <hpfull.shintaro@gmail.com>
2022-08-04 21:46:22 +0900

    works のリンク追加

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export const Works = () => {
@@ -9,48 +10,60 @@ export const Works = () => {
       </div>
       <div className='flex justify-between'>
         <div className='text-center w-full p-3'>
-          <div className='overflow-hidden'>
-            <Image
-              style={{ transition: 'transform .6s ease' }}
-              className='hover:transform hover:scale-110'
-              src='/undraw_Experience_design_re_ca7l.png'
-              width={256}
-              height={164}
-              alt='Avatar'
-            />
-          </div>
+          <Link href='https://github.com/hpfull-yamucha/react-redux-chatapp'>
+            <a target='_blank'>
+              <div className='overflow-hidden'>
+                <Image
+                  style={{ transition: 'transform .6s ease' }}
+                  className='hover:transform hover:scale-110'
+                  src='/undraw_Experience_design_re_ca7l.png'
+                  width={256}
+                  height={164}
+                  alt='Avatar'
+                />
+              </div>
+            </a>
+          </Link>
           <p className='text-gray-600 font-sans mb-2'>React Chat App</p>
           <p className='text-gray-600 font-sans text-left'>
             React,TypeScriptとReduxで構築された同期的なチャットアプリケーションです。チャットアプリの基本的な機能に加え、メッセージへのリアクション機能があります。
           </p>
         </div>
         <div className='text-center w-full p-3'>
-          <div className='overflow-hidden'>
-            <Image
-              style={{ transition: 'transform .6s ease' }}
-              className='hover:transform hover:scale-110'
-              src='/undraw_Programming_re_kg9v.png'
-              width={256}
-              height={164}
-              alt='Avatar'
-            />
-          </div>
+          <Link href='https://corp.hpfull.jp/blogs'>
+            <a target='_blank'>
+              <div className='overflow-hidden'>
+                <Image
+                  style={{ transition: 'transform .6s ease' }}
+                  className='hover:transform hover:scale-110'
+                  src='/undraw_Programming_re_kg9v.png'
+                  width={256}
+                  height={164}
+                  alt='Avatar'
+                />
+              </div>
+            </a>
+          </Link>
           <p className='text-gray-600 font-sans mb-2'>JAMstack Blog</p>
           <p className='text-gray-600 font-sans text-left'>
             Next.jsとヘッドレスCMSであるmicroCMSで構築されたブログサイトです。検索機能やページネーション機能、カテゴリ機能を自前で実装しています。
           </p>
         </div>
         <div className='text-center w-full p-3'>
-          <div className='overflow-hidden'>
-            <Image
-              style={{ transition: 'transform .6s ease' }}
-              className='hover:transform hover:scale-110'
-              src='/undraw_Group_video_re_btu7.png'
-              width={256}
-              height={164}
-              alt='Avatar'
-            />
-          </div>
+          <Link href='https://github.com/hpfull-yamucha/sample-portfolio'>
+            <a target='_blank'>
+              <div className='overflow-hidden'>
+                <Image
+                  style={{ transition: 'transform .6s ease' }}
+                  className='hover:transform hover:scale-110'
+                  src='/undraw_Group_video_re_btu7.png'
+                  width={256}
+                  height={164}
+                  alt='Avatar'
+                />
+              </div>
+            </a>
+          </Link>
           <p className='text-gray-600 font-sans mb-2'>portfolio site</p>
           <p className='text-gray-600 font-sans text-left'>
             本サイトはNext.js、Tailwind CSS、TypeScriptで構築されています。
